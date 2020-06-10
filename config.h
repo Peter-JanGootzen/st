b34a614cbc7b42dae598080b0f3c5f2e972058af
Peter-Jan Gootzen <peterjan@gootzen.net>
2020-06-10 21:02:29 +0200

    Changed font to Hasklig and applied w3m images patch

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "xos4 Terminus:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Hasklig:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
